3a7ac23806052cfd2570f3fc94309cff7135db8f
Marcus Kruse <esurk.sucram@gmail.com>
2022-11-19 09:51:10 +0100

    Require Elixir 1.12

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule GlobEx.MixProject do
     [
       app: :glob_ex,
       version: "0.1.0",
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       preferred_cli_env: preferred_cli_env(),
